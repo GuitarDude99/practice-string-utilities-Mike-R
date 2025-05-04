50bf8d25f798762236d5733b87c05c835b6f6b8c
Mike <007mrice@gmail.com>
2025-05-04 17:15:36 -0500

    add app.js

---
+++ string-utilities/app.js
@@ -0,0 +1,29 @@
+
+
+const {caps, reverse, contains} = require('./stringUtils.js');
+
+
+
+
+
+
+//Tests and Edge Cases
+
+console.log(caps("mike"));
+
+console.log(caps());
+
+console.log(reverse("mike"));
+
+console.log(reverse());
+
+console.log(contains("Mike", "ike"));
+
+console.log(contains("batman", "dog"));
+
+
+
+
+
+
+
