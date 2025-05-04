9b5f7bff9b5e31966a2a8a111591dcfca37e2a6e
Mike <007mrice@gmail.com>
2025-05-04 17:14:18 -0500

    add stringUtils.js

---
+++ string-utilities/stringUtils.js
@@ -0,0 +1,50 @@
+//Objectives:
+
+/* Create the stringUtils.js Module:
+
+Define and export the following functions:
+
+capitalize(str): Converts the first letter of a string to
+uppercase.
+
+reverse(str): Reverses the characters in a string.
+
+contains(str, substr): Checks if a substring exists within a
+string and returns true or false. 
+*/
+
+
+//Capitalize
+
+ function caps(str) {
+   if (typeof str !== "string" || str.length === 0) {
+      console.log("invalid input");
+      return "invalid input";
+ }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+ 
+
+
+//Reverse
+
+ function reverse(str) {
+   if (typeof str !== "string" || str.length === 0) {
+      console.log("invalid input");
+      return "invalid input";
+ }
+    return str.split('').reverse().join("");
+}
+
+
+
+//Checks for substring in string, return true or false
+
+ function contains(str, substr) {
+    if (!str.includes(substr)) {
+      return console.log("substring not found")  
+    }
+    return str.includes(substr)
+}
+
+module.exports = {caps, reverse, contains };
